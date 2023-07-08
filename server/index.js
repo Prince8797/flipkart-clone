ae07a4c6f3b35d03f0d2ec454d334308b02fbc0b
Prince_Mathur <princemathur8797@gmail.com>
2023-07-09 01:18:47 +0530

    deploy

--- server/index.js
+++ server/index.js
@@ -5,7 +5,9 @@ import defaultData from './defaultdata.js';
 import router from './routes/routes.js';
 import cors from 'cors';
 import bodyParser from 'body-parser';
+import path from 'path'
 
+const __dirname = path.resolve();
 const app = express()   // initialize the express js by using app varible as a function
 
 app.use(cors());   // this will help in connecting port 80 and port 3000 (backend and frontend port)
@@ -18,6 +20,11 @@ const USERNAME = process.env.DB_USERNAME;   // accessing data from .env file
 const PASSWORD = process.env.DB_PASSWORD;
 
 Connection(USERNAME, PASSWORD);    // passed username and password of .env file to Connection of db.js
+
+app.use(express.static(path.join(__dirname, '/shopyfy/build')));
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '/shopyfy/build/index.html'))
+});
 
 const PORT = 80;
 app.listen(PORT, () => console.log(`Server is running successfully on PORT ${PORT}`));
